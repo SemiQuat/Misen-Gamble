263e692c91a081eed58247cb3b2259c3678ddd22
SemiQuat <quatdude@gmail.com>
2023-11-29 17:24:49 -1000

    new import

---
+++ main.py
@@ -0,0 +1,21 @@
+import time
+import customtkinter
+
+class App(customtkinter.CTk):
+    def __init__(self):
+        super().__init__()
+        self.geometry("600x500")
+        self.title("CTk example")
+
+        
+        self.button = customtkinter.CTkButton(self, command=self.button_click)
+        self.button.grid(row=0, column=0, padx=20, pady=10)
+
+    
+    def button_click(self):
+        print("button click")
+
+
+window = App()
+window.mainloop()
+
