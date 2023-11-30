de075f6097c6d648e753516869766840adcf55e7
SemiQuat <quatdude@gmail.com>
2023-11-29 17:37:06 -1000

    import change

--- main.py
+++ main.py
@@ -1,14 +1,2 @@
 import time
-import customtkinter
-
-class App(customtkinter.CTk):
-    def __init__(self):
-        super().__init__()
-        self.geometry("500x500")
-        self.title("Misen Track")
-
-
-
-window = App()
-window.mainloop()
-
+import tkinter 
